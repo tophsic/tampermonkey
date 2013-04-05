e26de0841ab84109a9c97bd1b6524916260e4229
Christophe Sicard <sicard.christophe@gmail.com>
2013-04-05 17:09:27 +0200

    Basic dev on Help and Combo

--- Combo.js
+++ Combo.js
@@ -1,3 +1,6 @@
-Combo = function () {
-    return {};
+var Combo = {
+    create: function (c) {
+        keypress.register_combo(c);
+        Help.add(c);
+    }
 };
--- Help.js
+++ Help.js
@@ -1,3 +1,25 @@
-Help = function () {
-    return {};
+$('body').append('<div id="myHelp" title="Help box" ng-controller="HelpCtrl"><table></table></div>');
+
+var Help = {
+    add: function(c) {
+        $('#myHelp table').append('<tr><td>'+c.keys+'</td><td>'+c.description+'</td></tr>');
+    }
 };
+
+keypress.register_combo({ // Help
+    "keys"          : "shift ~",
+    "is_exclusive"  : true,
+    "on_keyup"      : function(e) {
+        
+        $('#myHelp').dialog({
+            modal: true,
+            buttons: {
+                Ok: function() {
+                    $( this ).dialog( "close" );
+                }
+            }
+        });
+        
+    },
+    "this"          : window
+});
